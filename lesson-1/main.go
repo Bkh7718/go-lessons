e104bcde7bfcdbd24c27653f84c3ff6db47e581d
Behruz Kholikov <behruzkholikov@gmail.com>
2025-11-09 20:52:43 +0300

    DZ-1 and DZ-2 done

--- lesson-1/main.go
+++ lesson-1/main.go
@@ -1,8 +1,14 @@
 package main
 
 func main() {
+	
+	//DZ-1
+	println("Hello Go") // Простейшая функция для вывода информации в терминал в нашем случае Hello GO
+	
+	//DZ-2
 	amount := 100.0 
-	buyRate := 10.3150
-	result := amount * buyRate
-	println(result)	
+	sellRate := 0.1470
+	result := amount / sellRate
+	println(result)	// Простейшие арифметические вычесления для конвертирования TJS в RUB в соответствии с курсом от лекции DZ-2 
+	
 }
