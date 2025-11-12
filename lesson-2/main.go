a4f39590e00b4e131de6805f462e656480441bb9
Behruz Kholikov <behruzkholikov@gmail.com>
2025-11-12 16:44:19 +0300

    DZ-3 and DZ-4 done

--- lesson-2/main.go
+++ lesson-2/main.go
@@ -1,5 +1,23 @@
 package main
 
 func main() {
-	println("Lesson-2")
+/*
+	amount := 999_999_999
+	minPercent := amount * 4 / 100 
+	maxPercent := amount * 6 / 100
+	minIncome := minPercent * 100 // DZ-3 Требует что бы вся сумма должна быть в дираммах из за этого умножаем на 100  
+	maxIncome := maxPercent * 100
+	println(minIncome, "diram")
+	println(maxIncome, "diram")
+*/
+
+	amount := 666
+	welcomeBonus := 5 
+
+	bonus := amount / 5 + welcomeBonus
+	println(bonus)
+
+
+
+
 }
